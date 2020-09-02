b9683d6a29111365d688a3228334fea792fb6948
YuktiJain123 <69969529+YuktiJain123@users.noreply.github.com>
2020-09-02 17:28:33 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -21,14 +21,17 @@ function preload() {
 function setup() {
   createCanvas(400, 400);
   
-  monkey =createSprite(50,180,20,50);
-  monkey.addAnimation("running",monkey1 );
-  monkey.scale = 0.5;
-  
   ground = createSprite(200,180,400,20);
   ground.addImage("ground",groundImage);
   ground.x = ground.width /2;
   ground.velocityX = -4;
+  
+  
+  monkey =createSprite(50,180,20,50);
+  monkey.addAnimation("running",monkey1 );
+  monkey.scale = 0.2;
+  
+  
   
   
   bananaGroup = new Group();
@@ -38,22 +41,25 @@ function setup() {
 }
 
 function draw() {
+  edges=createEdgeSprites();
   background(220);
   fill("black");
   textSize("20");
   stroke("white");
-  text("Score :+ score,500,50");
+  text("Score :"+ score,500,50);
    score = score + Math.round(getFrameRate()/60);
   spawnbanana();
   spawnObstacles();
-  
+ if (ground.x<0){
+   ground.x=ground.width/2
+ }
   if(keyDown("space")) {
     monkey.velocityY = -10;
   }
   
   monkey.velocityY = monkey.velocityY + 0.8
   
-  monkey.collide(ground);
+  monkey.collide(edges[3]);
  
   
   drawSprites();
@@ -62,32 +68,31 @@ function draw() {
 function spawnbanana()  {
   //write code here to spawn the clouds
   if (frameCount % 60 === 0) {
-    var banana = createSprite(600,120,40,10);
-    banana.y = Math.round(random(80,120));
+    var banana = createSprite(600,320,40,10);
+    banana.y = Math.round(random(180,320));
     banana.addImage(bananaImage);
     banana.scale = 0.05;
     banana.velocityX = -3;
     
-     //assign lifetime to the variable
+     //assign lifetime to the variable 
     banana.lifetime = 200;
     
-    //adjust the depth
-    banana.depth = monkey.depth;
-    monkey.depth = monkey.depth + 1;
+    
+    
     
     //add each cloud to the group
     bananaGroup.add(banana);
   }
 }
 function spawnObstacles() {
-  if(frameCount % 60 === 0) {
-    var obstacle = createSprite(600,165,10,40);
+  if(frameCount % 160 === 0) {
+    var obstacle = createSprite(600,365,10,40);
     obstacle.velocityX = -4;
-    obstacle.scale=0.08;
+    
     obstacle.addImage(obstacleImage);
    
 //assign scale and lifetime to the obstacle           
-    obstacle.scale = 0.5;
+    obstacle.scale = 0.2;
     obstacle.lifetime = 300;
     //add each obstacle to the group
     obstaclesGroup.add(obstacle);
